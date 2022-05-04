ebb3d65da76a223d2408957460c6e165554ab138
octavio telles teichner <ott.html@gmail.com>
2022-05-04 01:09:26 -0300

    add toggable widget

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,5 +1,23 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
+
 export function Home() {
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+    function toggleWidgetVisibility() {
+        setIsWidgetOpen(!isWidgetOpen);
+    }
+
     return (
-        <h1>Hello World</h1>
+        <div className='absolute bottom-4 right-4'>
+            {isWidgetOpen ? <p>Hello World</p> : null}
+
+            <button onClick={toggleWidgetVisibility} className='bg-violet-500 rounded-full p-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6'/>
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                    Feedback
+                </span>
+            </button>
+        </div>
     )
 }
