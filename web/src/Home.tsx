fa6f14b9ffe1ef99529aabd234d54b1366f5256c
octavio telles teichner <ott.html@gmail.com>
2022-05-04 01:19:14 -0300

    add accessibility to button using Popover from headlessui

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,23 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Home() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen);
-    }
 
     return (
-        <div className='absolute bottom-4 right-4'>
-            {isWidgetOpen ? <p>Hello World</p> : null}
+        <Popover className='absolute bottom-4 right-4'>
+            <Popover.Panel>Hello world</Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className='bg-violet-500 rounded-full p-3 h-12 text-white flex items-center group'>
+            <Popover.Button className='bg-violet-500 rounded-full p-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6'/>
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
