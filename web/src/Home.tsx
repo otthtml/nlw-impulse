068a3e802c097c4535d9f80a08a37711a2904e57
octavio telles teichner <ott.html@gmail.com>
2022-05-05 01:32:18 -0300

    add button to return to feedbacktypes selection

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,4 +1,4 @@
-import { BugBeetle, ChatTeardropDots, Lamp, Question, X } from 'phosphor-react'
+import { ArrowLeft, BugBeetle, ChatTeardropDots, Lamp, Question, X } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
 import { useState } from 'react'
 
@@ -14,21 +14,21 @@ const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
-            source: <BugBeetle />,
+            source: <BugBeetle className='w-6 h-6' />,
             alt: 'Imagem de um bug'
         }
     },
     IDEA: {
         title: 'Ideia',
         image: {
-            source: <Lamp />,
+            source: <Lamp className='w-6 h-6' />,
             alt: 'Imagem de uma lâmpada'
         }
     },
     OTHER: {
         title: 'Outro',
         image: {
-            source: <Question />,
+            source: <Question className='w-6 h-6' />,
             alt: 'Imagem de um interrogação'
         }
     }
@@ -36,31 +36,80 @@ const feedbackTypes = {
 
 type FeedbackType = keyof typeof feedbackTypes;
 
-export function WidgetForm() {
-    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+interface FeedbackTypeStepProps {
+    onFeedbackTypeChange: (feedbackType: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep(props: FeedbackTypeStepProps) {
     return (
-        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]'>
+        <>
             <header>
                 <span className='text-xl leading-6'>Deixe seu feedback</span>
                 <CloseButton/>
             </header>
+            <div className='flex py-8 gap-2 w-full'>
+                { Object.entries(feedbackTypes).map( (item) => {
+                    return (
+                        <button
+                            key={item[0]}
+                            className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-violet-500 focus:border-violet-500 focus:outline-none'
+                            onClick={() => props.onFeedbackTypeChange(item[0] as FeedbackType)}
+                            type='button'
+                        >
+                            {item[1].image.source}
+                            <span>{item[1].title}</span>
+                        </button>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
 
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+    returnToFeedbackType: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, returnToFeedbackType }: FeedbackContentStepProps) {
+    const feedbackInfo = feedbackTypes[feedbackType];
+
+    return(
+        <>
+            <header>
+                <button type='button'
+                    className='top-5 left-5 absolute text-zinc-400 hover:text-zinc-100'
+                    onClick={returnToFeedbackType}
+                >
+                    <ArrowLeft weight='bold' className='w-4 h-4' />
+
+                </button>
+
+                <span className='text-xl leading-6 flex items-center gap-2'>
+                    {feedbackInfo.image.source}
+                    {feedbackInfo.title}
+                </span>
+                <CloseButton/>
+            </header>
+            <div className='flex py-8 gap-2 w-full'>
+            </div>
+        </>
+    )   
+}
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function returnToFeedbackType() {
+        setFeedbackType(null);
+    }
+
+    return (
+        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto] text-white'>
             {!feedbackType ? (
-                <div className='flex py-8 gap-2 w-full'>
-                    { Object.entries(feedbackTypes).map( (item) => {
-                        return (
-                            <button
-                                key={item[0]}
-                                className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-violet-500 focus:border-violet-500 focus:outline-none'
-                                onClick={() => setFeedbackType(item[0] as FeedbackType)}
-                                type='button'
-                            >
-                                {item[1].image.source}
-                            </button>
-                        )
-                    })}
-                </div> ) : (
-                    <p>Hello World</p>
+                <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep feedbackType={feedbackType} returnToFeedbackType={returnToFeedbackType} />
             )}
 
             <footer className='text-xs text-neutral-400'>
