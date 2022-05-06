934636a9920da647022dcf33b7fe962d02756e36
octavio telles teichner <ott.html@gmail.com>
2022-05-06 02:14:26 -0300

    add CORS in order to avoid unexpected origins to access our backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes';
 
 const app = express();
-app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
