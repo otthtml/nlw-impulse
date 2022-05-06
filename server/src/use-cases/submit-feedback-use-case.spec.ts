550452f999d31d712de7ef9ca079e69b7759422b
octavio telles teichner <ott.html@gmail.com>
2022-05-06 02:10:34 -0300

    add unit tests for SubmitFeedbackUseCase

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,38 @@
-test('sum 2+2', () => {
-    expect(2 + 2).toBe(4);
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy }
+);
+
+describe('Submit feedback use case', () => {
+    it('should submit feedback', async () => {
+        await expect(submitFeedbackUseCase.execute({
+            type: 'BUG',
+            comment: 'test',
+            screenshot: 'data:image/png;base64,test.jpg'
+        })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalled();
+        expect(sendMailSpy).toHaveBeenCalled();
+    });
+
+    it('should not submit feedback without type or comment', async () => {
+        await expect(submitFeedbackUseCase.execute({
+            type: '',
+            comment: '',
+            screenshot: 'data:image/png;base64,test.jpg'
+        })).rejects.toThrow();
+    });
+
+    it('should not submit feedback with invalid screenshot', async () => {
+        await expect(submitFeedbackUseCase.execute({
+            type: 'BUG',
+            comment: 'test',
+            screenshot: 'test.jpg'
+        })).rejects.toThrow();
+    });
 });
