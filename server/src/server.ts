5c65cb02773ac2bbd7541ddecd5786895d93bfd0
octavio telles teichner <ott.html@gmail.com>
2022-05-06 00:32:15 -0300

    add mail feature to feedback endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,36 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 app.use(express.json());
 
-app.post('/feedback', (req, res) => {
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "1f28f6ff7bbb84",
+      pass: "484933a1606286"
+    }
+  });
+
+app.post('/feedback', async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
-    const feedback = prisma.feedback.create({
+    const feedback = await prisma.feedback.create({
         data: {
             type,
             comment,
             screenshot,
         }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <fake@feedget.com>',
+        to: 'Octavio <ott.html@gmail.com>',
+        subject: 'Feedback do usuário',
+        html: `<p>Tipo do feedback: ${type}</p>` +
+            `<p>Comentário: ${comment}</p>`
     });
     return res.status(201).json({ data: feedback });
 });
